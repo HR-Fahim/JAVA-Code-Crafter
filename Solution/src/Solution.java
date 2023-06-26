f6b03904110109e054cef2018bee8706f7443b7a
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-06-27 03:06:15 +0600

    Update Files

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,186 +1,53 @@
 
-import java.util.Scanner;
-import org.apache.commons.lang3.StringUtils;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-class CCClass {
+class sum{
+	public int sum(int[] ints){
+		  
+		  //sum the integers
 
-	public CCClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
+		  //return the sum
 		
-		for (int i = 0; i < str.length(); i++) {
-
-			if (str.charAt(0) == 'C' && str.charAt(2) == 'C' && i > 3) {
-
-				if (i == 4) {
-					spacedWord.append(str.toUpperCase().charAt(4));
-				}
-				else if (Character.isSpaceChar(str.charAt(i))) {
-
-					spacedWord.append(str.toUpperCase().charAt(i + 1));
-					for (int j = i + 2; j < str.length(); j++) {
-						spacedWord.append(str.charAt(j));
-					}
-					break;
-				}
-				else if (i > 4) spacedWord.append(str.charAt(i));
-			}
-		}
-		String result = spacedWord.toString();
-		System.out.println(result);
+		int sum = 0;
+        
+        // Calculating the sum of the elements in the array
+        for (int i = 0; i < ints.length; i++) {
+            sum += ints[i];
+        }
+        //System.out.print(ints.length);
+        return sum;
 	}
-
-}
-
-class CMClass {
-
-	public CMClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
-		
-		String[] chars = StringUtils.split(str);
-
-		for (String c : chars) {
-			spacedWord.append(StringUtils.capitalize(c.toLowerCase()));
-		}
-
-		String result = spacedWord.toString();
-		for(int i = 4 ; i < result.length() ; i++) {
-			System.out.print(result.charAt(i));
-		}
-		System.out.print("()");
-		System.out.println();
-	}
-
-}
-class CVClass {
-
-	public CVClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
-		
-		String[] chars = StringUtils.split(str);
-
-		for (String c : chars) {
-			spacedWord.append(StringUtils.capitalize(c.toLowerCase()));
-		}
-
-		String result = spacedWord.toString();
-		for(int i = 4 ; i < result.length() ; i++) {
-			System.out.print(result.charAt(i));
-		}
-		//System.out.print("()");
-		System.out.println();
-	}
-
-}
-class SCClass {
-
-	public SCClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
-		
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(0) == 'S' && str.charAt(2) == 'C' && i > 3) {
-				if (i == 4) {
-					spacedWord.append(str.toLowerCase().charAt(i));					
-				}				
-				else if (i > 4 && Character.isUpperCase(str.charAt(i))) {
-					spacedWord.append(" ");
-					spacedWord.append(str.toLowerCase().charAt(i));
-				}
-				else if (i > 4) {
-					spacedWord.append(str.charAt(i));
-				}
-			}				
-		}
-		String result = spacedWord.toString();
-		System.out.println(result);
-	}
-
-}
-class SMClass {
-
-	public SMClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
-		
-		str = str.replaceAll("[\\(\\)\\[\\]]", ""); // Keep updated str into str
-
-		for (int i = 0; i < str.length(); i++) {			
-
-			if (str.charAt(0) == 'S' && str.charAt(2) == 'M' && i > 3) {
-
-				if (Character.isUpperCase(str.charAt(i))) {
-
-					spacedWord.append(" "); 
-				}
-				spacedWord.append(str.toLowerCase().charAt(i)); // Append the current character
-			}
-		}
-		String result = spacedWord.toString(); // Append convert & print
-        System.out.println(result);
-	}
-
-}
-class SVClass {
-
-	public SVClass(String str) {
-		// TODO Auto-generated constructor stub
-		StringBuilder spacedWord = new StringBuilder();
-
-		for (int i = 0; i < str.length(); i++) {			
-
-			if (str.charAt(0) == 'S' && str.charAt(2) == 'V' && i > 3) {
-
-				if (Character.isUpperCase(str.charAt(i))) {
-
-					spacedWord.append(" "); 
-				}
-				spacedWord.append(str.charAt(i)); // Append the current character
-			}
-		}
-		String result = spacedWord.toString(); // Append convert & print
-        System.out.println(result.toLowerCase());
-	}
-
 }
 
 public class Solution {
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        String str;
-
-        while (true) {
-
-            if (scanner.hasNextLine()) {
-                str = scanner.nextLine();
-
-                if (str.isEmpty()) {
-                    break; // Exit the loop if no input is available
-                }
-
-                str = str.trim(); // Trim leading/trailing whitespace
-
-                if (str.charAt(0) == 'S' && str.charAt(2) == 'M') {
-                    SMClass SM = new SMClass(str);
-                } else if (str.charAt(0) == 'C' && str.charAt(2) == 'V') {
-                    CVClass CV = new CVClass(str);
-                } else if (str.charAt(0) == 'C' && str.charAt(2) == 'C') {
-                    CCClass CC = new CCClass(str);
-                } else if (str.charAt(0) == 'S' && str.charAt(2) == 'C') {
-                    SCClass SC = new SCClass(str);
-                } else if (str.charAt(0) == 'C' && str.charAt(2) == 'M') {
-                    CMClass CM = new CMClass(str);
-                } else if (str.charAt(0) == 'S' && str.charAt(2) == 'V') {
-                    SVClass SV = new SVClass(str);
-                }
-            } else {
-                break; // Exit the loop if no input is available
-            }
-        }
-        scanner.close(); // Close the scanner when done
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    	
+    	sum n = new sum();
+    	
+    	
+    	
+    	Scanner input = new Scanner(System.in);
+    	
+    	String str = input.nextLine();
+    	str = str.replaceAll("\\s", "");
+    	//System.out.println(str.length());
+    	int[] ints = new int[str.length()] ;
+    	
+    	for(int i = 0 ; i < str.length() ; i++) {
+    		if(Character.isDigit(str.charAt(i))) {
+    			ints[i] = Character.getNumericValue(str.charAt(i));
+    			//System.out.println(ints[i]);
+    		}
+    		else {
+    			System.out.print(str.charAt(i));
+    		}
+    	}
+    	int x = n.sum(ints);
+    	System.out.print(x);
     }
 }
