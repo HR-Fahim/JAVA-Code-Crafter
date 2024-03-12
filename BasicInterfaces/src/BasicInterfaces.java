6ba0f2d4bd87539bc38acef685f8edc2062e5b7d
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-03-12 07:51:22 +0600

    Delete unused index files and update version.ini

--- BasicInterfaces/src/BasicInterfaces.java
+++ BasicInterfaces/src/BasicInterfaces.java
@@ -16,7 +16,7 @@ class Calculator implements AdvancedArithmetic {
     }
 }
 
-class Solution {
+class BasicInterfaces {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
