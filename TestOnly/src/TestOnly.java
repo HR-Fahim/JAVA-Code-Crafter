3de696426978d6da076a57507fd9e912b87c6849
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2025-04-05 06:49:41 +0600

    Clean up unused index files and update version.ini

---
+++ TestOnly/src/TestOnly.java
@@ -0,0 +1,13 @@
+
+public class TestOnly {
+	public static void main(String args[]) {
+//		System.out.println("Hello World");
+		
+		int a = 45;
+		int b = 7;
+		
+		int value = a & (b-1);
+		
+		System.out.println(value);
+	}
+}
